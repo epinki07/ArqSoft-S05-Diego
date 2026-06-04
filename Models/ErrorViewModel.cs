2f32558a6d0cdad9e75f442608206ef8ee23f5cc
epinki07 <dramirezmagana@gmail.com>
2026-06-03 22:19:16 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArqSoft_S05_Diego.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
