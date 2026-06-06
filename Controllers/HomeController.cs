1019e4e06892b6c27ceb31b144589ef48ed245b4
epinki07 <dramirezmagana@gmail.com>
2026-06-05 21:28:14 -0600

    ADD: Se modifico el CSS para darle una mejor interfaz y facil uso para el usuario.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using ArqSoft_S05_Diego.Interfaces;
 using ArqSoft_S05_Diego.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,8 +7,29 @@ namespace ArqSoft_S05_Diego.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPacienteRepository _pacienteRepo;
+        private readonly IMedicoRepository _medicoRepo;
+        private readonly ICitaRepository _citaRepo;
+
+        public HomeController(
+            IPacienteRepository pacienteRepo,
+            IMedicoRepository medicoRepo,
+            ICitaRepository citaRepo)
+        {
+            _pacienteRepo = pacienteRepo;
+            _medicoRepo = medicoRepo;
+            _citaRepo = citaRepo;
+        }
+
         public IActionResult Index()
         {
+            var citas = _citaRepo.ObtenerTodos().ToList();
+
+            ViewBag.TotalPacientes = _pacienteRepo.ObtenerTodos().Count();
+            ViewBag.TotalMedicos = _medicoRepo.ObtenerTodos().Count();
+            ViewBag.TotalCitas = citas.Count;
+            ViewBag.CitasPendientes = citas.Count(c => c.Estado == "Pendiente");
+
             return View();
         }
 
